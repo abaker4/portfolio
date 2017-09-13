e4563e1b208556230c9a92f54f45fc52f0c384e9
Austin Baker <baker.austin.c@gmail.com>
2017-09-13 14:23:10 -0400

    various refactors

--- config/app.php
+++ config/app.php
@@ -12,7 +12,7 @@ return [
     | any other location as required by the application or its packages.
     */
 
-    'name' => env('APP_NAME', 'The Veil Brewing Co.'),
+    'name' => env('APP_NAME', 'Portfolio'),
 
     /*
     |--------------------------------------------------------------------------
@@ -162,7 +162,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        Cornford\Googlmapper\MapperServiceProvider::class,
+
 
 
         /*
@@ -227,7 +227,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'Mapper' => Cornford\Googlmapper\Facades\MapperFacade::class,
+
 
     ],
 
